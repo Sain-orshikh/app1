b9789f6218b2bc825168415e792b592891b8cca8
Sain-orshikh <sainorshikh1121@gmail.com>
2024-11-12 19:22:35 +0800

    .

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,15 @@ dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000 ");
+    console.log("Server started at http://localhost:"+ PORT);
 });
 
 //bjI5A945rV0ZU65A
