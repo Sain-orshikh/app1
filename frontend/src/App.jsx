6a0675872378017629b131bee809efd6b473bb2d
Sain-orshikh <162454448+Sain-orshikh@users.noreply.github.com>
2024-11-27 19:32:37 +0800

    I set up mui succesfully

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ButtonGroup, Button } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='flex flex-center'>Hello</div>
+      <div className='flex flex-center'>
+        <ButtonGroup>
+          <Button>Hi!</Button>
+          <Button>Hey!</Button>
+        </ButtonGroup>
+      </div>
     </>
   )
 }
