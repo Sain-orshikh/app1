5ba193db383b4bdbb0a7fd59353a13e1744ae879
Sain-orshikh <162454448+Sain-orshikh@users.noreply.github.com>
2024-11-27 19:24:30 +0800

    I finnaly set up tailwind!!!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,14 @@
-import { Box } from "@chakra-ui/react"
-import { Route, Routes } from "react-router-dom"
-
-import HomePage from "./pages/HomePage"
-import CreatePage from "./pages/CreatePage"
-import Navbar from "./components/Navbar"
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Box minH={"100vh"}>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<HomePage />}/>
-          <Route path="/create" element={<CreatePage />}/>
-        </Routes>
-      </Box>
+      <div className='flex flex-center'>Hello</div>
     </>
   )
 }
