b5e2549d286182521c56d951759f6cb64c1b5154
Sain-orshikh <162454448+Sain-orshikh@users.noreply.github.com>
2024-12-03 20:04:04 +0800

    finished navbar(except darkmode)

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
-import { ButtonGroup, Button, Box } from '@mui/material'
+import { ButtonGroup, Button, Box, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { CgAddR } from "react-icons/cg";
+import { TiAdjustBrightness, TiWeatherSunny } from "react-icons/ti";
 //import IsSmallScreen from '../modes/isSmallScreen'
 
 function Navbar() {
 
   return (
     <>
-      <Box className="flex flex-col sm:flex-row items-center justify-between">
-        <Button>Hihihiha</Button>
-        <Button>Hehe</Button>
+      <Box className="flex flex-col sm:flex-row justify-between items-center w-full mx-5 mt-3">
+        
+        <Button className=''>
+          <Link to={"/"}>
+            <Typography className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-500 font-bold">
+              Product Store 🛒
+            </Typography>
+          </Link>
+        </Button>
+        <ButtonGroup className='mr-5'>
+          <Button>
+            <Link to={'/create'}><CgAddR fontSize={20}/></Link>
+          </Button>
+          
+          <Button>
+            <TiAdjustBrightness fontSize={20}/>
+          </Button>
+        </ButtonGroup>
+
       </Box>
     </>
   )
